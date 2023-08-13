75e8d2003192e2613e0e8ad5c46f68217012dc9d
Yan_yx <u202215545@hust.edu.cn>
2023-08-13 22:36:42 +0800

    initial commit,switch part unfinished

---
+++ TestRoutine.c
@@ -0,0 +1,23 @@
+/***********************************************************************************************
+ * FILE: TestRoutine.c
+ * DESCRIPTION: 
+ * This file is used to test the functions in the project.
+ * AUTHOR: yyx
+ * LASTREVISED: 2023.8.13
+************************************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    int a=10;
+    float b=4.3;
+    double c=5.2;
+    char d='a';
+
+    int arr1[10];
+    float arr2[10];
+    double arr3[10];
+    char arr4[10];
+    
+}
